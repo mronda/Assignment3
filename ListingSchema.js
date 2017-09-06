a48a9992eeabd464df5366b29ea48d59fe3d551c
Maximiliano Ronda <mronda11@gmail.com>
2017-09-06 17:03:44 -0400

    creating pre function

--- ListingSchema.js
+++ ListingSchema.js
@@ -12,11 +12,20 @@ var listingSchema = new Schema({
     longitude: Number
   },
   address: String
+  created_at: Date,
+  updated_at: Date
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
 listingSchema.pre('save', function(next) {
   /* your code here */
+  var currentDate = new Date();
+  this.updated_at = currentDate;
+  if(!this.created_at)
+  {
+    this.created_at = currentDate;
+  }
+  next();
 });
 
 /* Use your schema to instantiate a Mongoose model */
