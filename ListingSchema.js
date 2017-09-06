f50fdda0abb45463207b1bf5284c6c94b4928736
Maximiliano Ronda <mronda11@gmail.com>
2017-09-06 16:52:57 -0400

    adding schemas to listingSchema

--- ListingSchema.js
+++ ListingSchema.js
@@ -1,10 +1,17 @@
 /* Import mongoose and define any variables needed to create the schema */
-var mongoose = require('mongoose'), 
+var mongoose = require('mongoose'),
     Schema = mongoose.Schema;
 
 /* Create your schema */
 var listingSchema = new Schema({
   /* your code here */
+  code: {type: String, required: true, unique: true},
+  name: {type: String, required: true},
+  coordinates: {
+    latitude: Number,
+    longitude: Number
+  },
+  address: String
 });
 
 /* create a 'pre' function that adds the updated_at (and created_at if not already there) property */
